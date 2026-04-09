69f2e888f49036703b9113f2ab95ebad1ca46e05
BahriniHoussem001 <145834601+BahriniHoussem001@users.noreply.github.com>
2026-04-09 14:58:19 +0100

    Add Docker support for frontend, backend, and mysql

--- backend/gestion-achats-fournisseurs/src/main/java/com/gestionAchatfournisseur/repo/FournisseurRepository.java
+++ backend/gestion-achats-fournisseurs/src/main/java/com/gestionAchatfournisseur/repo/FournisseurRepository.java
@@ -2,7 +2,7 @@ package com.gestionAchatfournisseur.repo;
 
 import java.beans.JavaBean;
 
-import org.glassfish.jaxb.core.v2.model.core.ID;
+//import org.glassfish.jaxb.core.v2.model.core.ID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.gestionAchatfournisseur.entity.Fournisseur;
